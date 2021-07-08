111d9ad29414ff717a2f7a1af3411e2dfbabb5fc
Maksudkhanov <75378276+Maksudkhanov@users.noreply.github.com>
2021-07-08 18:54:52 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const {Telegraf} = require('telegraf')
 const {checkTest} = require('./commands/checkTest/checkTest')
 const start = require('./commands/start/start')
 
-const bot = new Telegraf('1880282912:AAG4CmayJKCFWbgB7DYYT6DZvnG8AEK-kio');
+const bot = new Telegraf('TOKEN');
 
 bot.start(async(ctx) => {
     await start(bot, ctx)  
