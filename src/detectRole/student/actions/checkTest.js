bf285a77b490900e52a106e5ea95210a2504c8ca
Maksudkhanov <azizxon.maksudxanov@mail.ru>
2021-07-09 18:31:23 +0500

    divided roles, maked skelet and working inline-keyword without logic

--- src/detectRole/student/actions/checkTest.js
+++ src/detectRole/student/actions/checkTest.js
@@ -1,11 +1,11 @@
 function checkTest(answers) {
-    let correct = 0
-    for (let i = 0; i < answers.length; i++) {
-       if(answers[i] === 'a') {
-           correct++
-       }
+  let correct = 0;
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === "a") {
+      correct++;
     }
-    return correct
+  }
+  return correct;
 }
 
-module.exports = checkTest
+module.exports = checkTest;
